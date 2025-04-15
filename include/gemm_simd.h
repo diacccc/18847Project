2556bfd97e57f046fa33ed927c5aa819659ac5f4
diacccc <935433034@qq.com>
2025-04-14 23:07:34 -0400

    8x8, 16x8 kernal for arm neon

--- include/gemm_simd.h
+++ include/gemm_simd.h
@@ -24,13 +24,25 @@ namespace gemm {
                 float *C, int LDC
             );  
 
-            void packing_A_8x4_neon(
+            void packing_A_8_neon(
                 const float* A, 
                 size_t M, size_t K, size_t LDA,
                 float *packed_A
             );
 
-            void packing_B_8x4_neon(
+            void packing_A_16_neon(
+                const float* A, 
+                size_t M, size_t K, size_t LDA,
+                float *packed_A
+            );
+
+            void packing_B_4_neon(
+                const float* B, 
+                size_t K, size_t N, size_t LDB,
+                float *packed_B
+            );
+
+            void packing_B_8_neon(
                 const float* B, 
                 size_t K, size_t N, size_t LDB,
                 float *packed_B
@@ -44,20 +56,26 @@ namespace gemm {
                 float beta, 
                 float *C, int LDC
             );
-
-            void packing_A_16x4_neon(
-                const float* A, 
-                size_t M, size_t K, size_t LDA,
-                float *packed_A
-            );
-
-            void packing_B_16x4_neon(
-                const float* B, 
-                size_t K, size_t N, size_t LDB,
-                float *packed_B
-            );
-
+            
             void macro_kernel_16x4_sgemm_neon(
+                size_t M, size_t N, size_t K, 
+                float alpha, 
+                const float *A, int LDA, 
+                const float *B, int LDB, 
+                float beta, 
+                float *C, int LDC
+            );
+
+            void macro_kernel_8x8_sgemm_neon(
+                size_t M, size_t N, size_t K, 
+                float alpha, 
+                const float *A, int LDA, 
+                const float *B, int LDB, 
+                float beta, 
+                float *C, int LDC
+            );
+
+            void macro_kernel_16x8_sgemm_neon(
                 size_t M, size_t N, size_t K, 
                 float alpha, 
                 const float *A, int LDA, 
--- src/implementations/gemm_simd.cpp
+++ src/implementations/gemm_simd.cpp
@@ -33,8 +33,8 @@ void GemmSIMD::execute(float alpha, const Matrix<float> &A, const Matrix<float> 
 
     scale(beta, C);
 
-    float *packed_A = new float[M * K];
-    float *packed_B = new float[K * N];
+    float *packed_A = (float *) aligned_alloc(4096,K_BLOCKING*M_BLOCKING*sizeof(float));
+    float *packed_B = (float *) aligned_alloc(4096,K_BLOCKING*N_BLOCKING*sizeof(float));
     size_t m_count, n_count, k_count;
     size_t m_inc, n_inc, k_inc;
     for (n_count = 0; n_count < N; n_count += n_inc)
@@ -43,14 +43,14 @@ void GemmSIMD::execute(float alpha, const Matrix<float> &A, const Matrix<float> 
         for (k_count = 0; k_count < K; k_count += k_inc)
         {
             k_inc = (K - k_count > K_BLOCKING) ? K_BLOCKING : K - k_count;
-            packing_B_16x4_neon(&B.at(k_count, n_count), k_inc, n_inc, B.ld(), packed_B);
+            packing_B_8_neon(&B.at(k_count, n_count), k_inc, n_inc, B.ld(), packed_B);
             for (m_count = 0; m_count < M; m_count += m_inc)
             {
                 m_inc = (M - m_count > M_BLOCKING) ? M_BLOCKING : N - m_count;
-                packing_A_16x4_neon(&A.at(m_count, k_count), m_inc, k_inc, A.ld(), packed_A);
+                packing_A_16_neon(&A.at(m_count, k_count), m_inc, k_inc, A.ld(), packed_A);
 #ifdef __APPLE__
-                macro_kernel_16x4_sgemm_neon(m_inc, n_inc, k_inc, alpha, packed_A, A.ld(), packed_B, B.ld(), beta,
-                                             &C.at(m_count, n_count), C.ld());
+                macro_kernel_16x8_sgemm_neon(m_inc, n_inc, k_inc, alpha, packed_A, A.ld(), packed_B, B.ld(), beta,
+                                            &C.at(m_count, n_count), C.ld());
 
 #else
                 macro_kernel_4x1_sgemm_intel(M, N, K, alpha, A.data(), A.ld(), B.data(), B.ld(), beta, C.data(),

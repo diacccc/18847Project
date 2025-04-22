83b0065be869c9a2f4d2faaca21fa151dab9c28b
Minn <minn798088@gmail.com>
2025-04-21 20:03:51 -0400

    omp: vectorized loop

--- src/implementations/gemm_omp.cpp
+++ src/implementations/gemm_omp.cpp
@@ -15,27 +15,37 @@ namespace gemm
 {
     void pack_block_A(const Matrix<float> &A, float* packed, size_t ib, size_t kb, size_t M, size_t K)
     {
+        // Get direct pointer to data for vectorization
+        const float* A_data = A.data();
+        const size_t LDA = A.ld();
+
         // Pack A for better cache locality - row-major order
         for (size_t m = 0; m < M; m++) {
+            // Use direct pointer access for better vectorization
             #pragma omp simd
             for (size_t k = 0; k < K; k++) {
-                packed[k + m * K] = A.at(ib + m, kb + k);
+                packed[k + m * K] = A_data[(ib + m) + (kb + k) * LDA];
             }
         }
     }
 
     void pack_block_B(const Matrix<float> &B, float* packed, size_t kb, size_t jb, size_t K, size_t N)
     {
+        // Get direct pointer to data for vectorization
+        const float* B_data = B.data();
+        const size_t LDB = B.ld();
+
         // Pack B for better cache locality - column-major order
         for (size_t n = 0; n < N; n++) {
+            // Use direct pointer access for better vectorization
             #pragma omp simd
             for (size_t k = 0; k < K; k++) {
-                packed[k + n * K] = B.at(kb + k, jb + n);
+                packed[k + n * K] = B_data[(kb + k) + (jb + n) * LDB];
             }
         }
     }
 
-    // SIMD-optimized kernel for matrix multiplication
+    // SIMD-optimized matrix multiplication
     void GemmOMP::execute(float alpha, const Matrix<float> &A, const Matrix<float> &B, float beta, Matrix<float> &C)
     {
         const size_t M = A.rows();
@@ -52,6 +62,10 @@ namespace gemm
 
         // Apply beta scaling to C
         scale(beta, C);
+
+        // Get direct pointer to C data for the final update
+        float* C_data = C.data();
+        const size_t LDC = C.ld();
 
         #pragma omp parallel
         {
@@ -76,13 +90,14 @@ namespace gemm
                             for (size_t jj = 0; jj < n_size; jj++) {
                                 float sum = 0.0f;
 
-                                // This is where SIMD vectorization happens
+                                // This is where SIMD vectorization happens effectively
                                 #pragma omp simd reduction(+:sum)
                                 for (size_t kk = 0; kk < k_size; kk++) {
                                     sum += packed_A[kk + ii * k_size] * packed_B[kk + jj * k_size];
                                 }
 
-                                C.at(i + ii, j + jj) += alpha * sum;
+                                // Direct pointer access for better performance
+                                C_data[(i + ii) + (j + jj) * LDC] += alpha * sum;
                             }
                         }
                     }
